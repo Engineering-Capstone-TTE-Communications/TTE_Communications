e6ff58303a1608b4a82c574744f39b2dd80c951c
David Lutzer <LutzerD@users.noreply.github.com>
2019-03-15 02:07:49 -0600

    Save point for video router

--- GUI/video_router.py
+++ GUI/video_router.py
@@ -46,6 +46,7 @@ def upsample_3d(x,L,order_num):
     for color in np.arange(1,num_colors):
         zoomed_color = scipy.ndimage.zoom(x[:,:,color], L, order=order_num)
         y = np.dstack((y,zoomed_color))
+        
     return y
 
 
@@ -56,18 +57,23 @@ def blur(frame,sigma):
     return frame
 
 
-def sharpen(frame,sigma): #highpass
+def sharpen(frame,sigma,scalar): #highpass
     l,w,num_colors = frame.shape
+    print(scalar)
+    temp_frame = frame
+    increase_contrast(temp_frame,1,1)
     for color in np.arange(num_colors):
-        frame_blurred_2d = ndimage.gaussian_filter(frame[:,:,color], sigma)
-        frame[:,:,color] = frame[:,:,color]/2 + frame_blurred_2d/2
+        #frame_blurred_2d = ndimage.gaussian_filter(frame[:,:,color], sigma)
+        frame[:,:,color] = frame[:,:,color]+scalar*(frame[:,:,color]- temp_frame[:,:,color])
     return frame
 
-def rx_filter(frame,L,order_num,sigma):
+def rx_filter(frame,L,order_num,sigma,sharpen_weighting):
     for i in np.arange(1,L,2):
         frame = upsample_3d(frame,2,order_num)
-        frame = blur(frame,sigma)        
-        
+        frame = blur(frame,sigma)
+        frame = sharpen(frame,sigma,sharpen_weighting)
+        #frame = blur(frame,sigma)/2+sharpen(frame,sigma,sharpen_weighting)/2
+
     return frame
     
 def blocking_fps_delay(frame_rate,start_time_ns):
@@ -81,9 +87,11 @@ def blocking_fps_delay(frame_rate,start_time_ns):
 def increase_contrast(frame,lim,L):#,clipLim,tileGridS):     
     l,w,num_colors = frame.shape
     clahe = cv2.createCLAHE(clipLimit=lim, tileGridSize=(L,L))
+    frame_out = frame
     for color in np.arange(num_colors):
-        frame[:,:,color] = clahe.apply(frame[:,:,color])
-    return frame
+        clahe.apply(frame_out[:,:,color])
+        pass
+    return frame_out
 
 #resize og to raw sized
 
@@ -118,16 +126,14 @@ while(1):
         frame_bit_count = sum((frame.shape)[0:1])*(frame.shape)[2]
 
         upsample_scalar = np.floor(np.log2(max_height/(frame_bit_count)))
-        #1 3 10
+        #1 3 10, magic voodoo numbers
         sigma = 10*((upsample_scalar)**-1)
-
-        print(downsample_scalar)
-        print(upsample_scalar)
-        print(sigma)
         
-        frame = rx_filter(frame,upsample_scalar,order_num,sigma)
+        sharpen_weighting = 100**-1
+        frame = rx_filter(frame,upsample_scalar,order_num,sigma,sharpen_weighting)
         
         display_frame(frame,str(order_num))
+
         blocking_fps_delay(frame_rate,start_time_ns)
         start_time_ns = time_lib.monotonic_ns()
 
