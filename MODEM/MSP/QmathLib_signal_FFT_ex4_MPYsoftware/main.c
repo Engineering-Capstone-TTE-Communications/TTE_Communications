f0188f91db61148670fca31c4b0e52a44f9ef971
David Lutzer <LutzerD@users.noreply.github.com>
2019-03-24 04:42:20 -0600

    8 MHz clk online

--- MODEM/MSP/QmathLib_signal_FFT_ex4_MPYsoftware/main.c
+++ MODEM/MSP/QmathLib_signal_FFT_ex4_MPYsoftware/main.c
@@ -113,7 +113,7 @@ void setup_adc(){
 
     // Configure ADC
     ADCCTL0 |= ADCON | ADCMSC;                                // ADCON
-    ADCCTL1 |= ADCSHS_2 | ADCCONSEQ_2;                        // repeat single channel; TB1.1 trig sample start
+    ADCCTL1 |= ADCSHS_2 | ADCCONSEQ_2 | ADCSSEL_3;                        // repeat single channel; TB1.1 trig sample start
     ADCCTL2 &= ~ADCRES;                                       // clear ADCRES in ADCCTL
     ADCCTL2 |= ADCRES_2;                                      // 12-bit conversion results
     ADCMCTL0 |= ADCINCH_1;                                    // A1 ADC input select; Vref=1.5V
@@ -127,11 +127,99 @@ void setup_adc(){
 
     // ADC conversion trigger signal - TimerB1.1 (32ms ON-period)
     TB1CCR0 = adc_PRESCALAR;                                         // PWM Period
-    TB1CCR1 = 1;                                          // TB1.1 ADC trigger
+    TB1CCR1 = (adc_PRESCALAR>>1);                                          // TB1.1 ADC trigger
     TB1CCTL1 = OUTMOD_6;                                      // TB1CCR0 toggle
     TB1CTL = TBSSEL__SMCLK | MC_1 | TBCLR;                     // ACLK, up mode)
 }
 
+#define MCLK_FREQ_MHZ 8                     // MCLK = 8MHz
+
+
+void Software_Trim()
+{
+    unsigned int oldDcoTap = 0xffff;
+    unsigned int newDcoTap = 0xffff;
+    unsigned int newDcoDelta = 0xffff;
+    unsigned int bestDcoDelta = 0xffff;
+    unsigned int csCtl0Copy = 0;
+    unsigned int csCtl1Copy = 0;
+    unsigned int csCtl0Read = 0;
+    unsigned int csCtl1Read = 0;
+    unsigned int dcoFreqTrim = 3;
+    unsigned char endLoop = 0;
+
+    do
+    {
+        CSCTL0 = 0x100;                         // DCO Tap = 256
+        do
+        {
+            CSCTL7 &= ~DCOFFG;                  // Clear DCO fault flag
+        }while (CSCTL7 & DCOFFG);               // Test DCO fault flag
+
+        __delay_cycles((unsigned int)3000 * MCLK_FREQ_MHZ);// Wait FLL lock status (FLLUNLOCK) to be stable
+                                                           // Suggest to wait 24 cycles of divided FLL reference clock
+        while((CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1)) && ((CSCTL7 & DCOFFG) == 0));
+
+        csCtl0Read = CSCTL0;                   // Read CSCTL0
+        csCtl1Read = CSCTL1;                   // Read CSCTL1
+
+        oldDcoTap = newDcoTap;                 // Record DCOTAP value of last time
+        newDcoTap = csCtl0Read & 0x01ff;       // Get DCOTAP value of this time
+        dcoFreqTrim = (csCtl1Read & 0x0070)>>4;// Get DCOFTRIM value
+
+        if(newDcoTap < 256)                    // DCOTAP < 256
+        {
+            newDcoDelta = 256 - newDcoTap;     // Delta value between DCPTAP and 256
+            if((oldDcoTap != 0xffff) && (oldDcoTap >= 256)) // DCOTAP cross 256
+                endLoop = 1;                   // Stop while loop
+            else
+            {
+                dcoFreqTrim--;
+                CSCTL1 = (csCtl1Read & (~DCOFTRIM)) | (dcoFreqTrim<<4);
+            }
+        }
+        else                                   // DCOTAP >= 256
+        {
+            newDcoDelta = newDcoTap - 256;     // Delta value between DCPTAP and 256
+            if(oldDcoTap < 256)                // DCOTAP cross 256
+                endLoop = 1;                   // Stop while loop
+            else
+            {
+                dcoFreqTrim++;
+                CSCTL1 = (csCtl1Read & (~DCOFTRIM)) | (dcoFreqTrim<<4);
+            }
+        }
+
+        if(newDcoDelta < bestDcoDelta)         // Record DCOTAP closest to 256
+        {
+            csCtl0Copy = csCtl0Read;
+            csCtl1Copy = csCtl1Read;
+            bestDcoDelta = newDcoDelta;
+        }
+
+    }while(endLoop == 0);                      // Poll until endLoop == 1
+
+    CSCTL0 = csCtl0Copy;                       // Reload locked DCOTAP
+    CSCTL1 = csCtl1Copy;                       // Reload locked DCOFTRIM
+    while(CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1)); // Poll until FLL is locked
+}
+
+int set_8mhz_clk(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+
+    __bis_SR_register(SCG0);                // disable FLL
+    CSCTL3 |= SELREF__REFOCLK;              // Set REFO as FLL reference source
+    CSCTL1 = DCOFTRIMEN_1 | DCOFTRIM0 | DCOFTRIM1 | DCORSEL_3;// DCOFTRIM=3, DCO Range = 8MHz
+    CSCTL2 = FLLD_0 + 243;                  // DCODIV = 8MHz
+    __delay_cycles(3);
+    __bic_SR_register(SCG0);                // enable FLL
+    Software_Trim();                        // Software Trim to get the best DCOFTRIM value
+
+
+    CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK; // set default REFO(~32768Hz) as ACLK source, ACLK = 32768Hz
+                                            // default DCODIV as MCLK and SMCLK source
+}
 void setup_dac(){
     WDTCTL = WDTPW + WDTHOLD;                 // Stop watch dog timer
 
@@ -332,7 +420,7 @@ int main(void){
     setup_adc();
     setup_dac();
     setup_roms();
-
+    set_8mhz_clk();
     init_USB();
 
     reset_rom_flags_phases(&ADC_signal);
